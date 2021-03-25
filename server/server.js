77fa53f5ad4dc5be96c64176f35927e349b4e01f
Adriano <adriano.sotos@linx3.com>
2021-03-25 18:55:32 -0300

    feat: refactor create and read, and added update and delete methods

--- server/server.js
+++ server/server.js
@@ -7,43 +7,90 @@ const PORT = 5000
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  Course.find({}, function (err, result) {
-    if (err) {
-      res.status(400).send({
-        'success': false,
-        'data': result
-      })
-    } else {
-      res.status(200).send({
-        'success': true,
-        'data': result
-      })
-    }
-  })
-});
-
 app.post('/', function (req, res) {
-  const newCourse = {
-    name: req.query.name,
-    duration: req.query.duration
-  }
+  const { name, duration } = req.query || {}
 
-  Course.create(newCourse, function (err, result) {
-    if (err) {
-      res.status(400).send({
-        'success': false,
-        'error': err.message
-      })
-    } else {
-      res.status(201).send({
-        'success': true,
-        'data': result,
-        'message': 'Course created successfully'
-      })
-    }
+  Course.create({
+    name,
+    duration
+  })
+  .then(result => {
+    res.status(201).send({
+      success: true,
+      data: result,
+      message: 'Course created successfully'
+    })
+  })
+  .catch(error => {
+    res.status(400).send({
+      sucess: false,
+      data: error
+    })
   })
 })
+
+app.get('/', function (req, res) {
+  Course.find({})
+  .then(result => {
+    res.status(200).send({
+      success: true,
+      data: result
+    })
+  })
+  .catch(error => {
+    res.status(400).send({
+      success: false,
+      data: error
+    })
+  })
+})
+
+app.put('/', function (req, res) {
+  const { id, name, duration } = req.query || {}
+
+  Course.findOneAndUpdate(
+    id,
+    {
+      $set: {
+        name,
+        duration
+      }
+    }
+  )
+  .then(result => {
+    res.status(200).send({
+      success: true,
+      data: result,
+      message: 'Course updated successfuly'
+    })
+  })
+  .catch(error => {
+    res.status(400).send({
+      success: false,
+      result: error
+    })
+  })
+})
+
+app.delete('/', function (req, res) {
+  const { id } = req.query || {}
+  Course.deleteOne({ _id: id })
+  .then(result => {
+    res.status(200).send({
+      success: true,
+      data: result,
+      message: 'Course deleted successfuly'
+    })
+  })
+  .catch(error => {
+    res.status(400).send({
+      success: false,
+      result: error
+    })
+  })
+})
+
+
 
 app.listen(PORT, function () {
   console.log(`Server Listening on ${PORT}`)
