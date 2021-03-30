0fc274b91eabb79b874c5b67b50225ed6e835509
Adriano <adriano.sotos@linx3.com>
2021-03-29 22:05:32 -0300

    feat: app routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import './styles/App.css';
+import { Route, HashRouter as Router, Switch, useHistory } from 'react-router-dom'
+import useFindUser from './hooks/useFindUsers'
+import { UserContext } from './hooks/userContext'
 import Dashboard from './pages/dashboard'
+import Login from './pages/login'
+import PrivateRoute from './pages/privateRoute'
+import Header from './components/header'
 
 function App() {
+  const { user, setUser } = useFindUser()
+
   return (
     <div className="App">
-      <Dashboard />
+      <Router>
+        <UserContext.Provider value={{ user, setUser }}>
+          <Header />
+          <Switch>
+            <Route path="/login" component={Login} exact />
+            <PrivateRoute path='/' component={Dashboard} exact />
+          </Switch>
+        </UserContext.Provider>
+      </Router>
     </div>
   );
 }
