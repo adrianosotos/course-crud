e68a1acec1c990e23cc0c78ac1f09808ba638bc2
Adriano <adriano.sotos@linx3.com>
2021-03-29 22:03:27 -0300

    feat: login component

---
+++ client/src/pages/login.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import Input from '../components/input'
+import useAuth from '../hooks/useAuth'
+
+function Login () {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+
+  function handleChange(e) {
+    const { name, value } = e.target
+
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
+
+  const { registerUser, loginUser, error } = useAuth()
+
+  async function handleRegister () {
+    await registerUser(user)
+  }
+
+  async function handleLogin () {
+    await loginUser(user)
+  }
+  
+  return (
+    <div>
+      <div>
+           { error && error.messages }
+      </div>
+      <form>
+        <Input 
+          label="Email"
+          name="email"
+          onChange={handleChange}
+        />
+        <Input 
+          label="Password"
+          name="password"
+          onChange={handleChange}
+        />
+      </form>
+      <button onClick={handleLogin}>Entrar</button>
+      <button onClick={handleRegister}>Inscrever</button>
+    </div>
+  )
+}
+
+export default Login
